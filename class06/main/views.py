cfb72e9b82a6197c4912e0ded42bd8240feb776c
kshittiz101 <chaudharykshittiz950@gmail.com>
2024-12-23 19:18:14 +0545

    class06

---
+++ class06/main/views.py
@@ -0,0 +1,45 @@
+from django.shortcuts import render
+from datetime import datetime
+
+# Create your views here.
+
+
+# def learn_django(request):
+# named argument
+#     return render(template_name="main/index.html", request=request, context={'name': 'kshittiz'})
+
+
+# def learn_django(request):
+#     context = {'name': 'Unnati'}
+#     # positional argument
+#     return render(request, "main/index.html", context)
+
+
+# def learn_django(request):
+#     # filter
+#     context = {'des': 'Nepal is beautiful country'}
+#     # positional argument
+#     return render(request, "main/index.html", context)
+
+
+# def learn_django(request):
+#     # datetime
+#     today_datatime = datetime.now()
+
+#     context = {
+#         'name': 'unnati',
+#         'dob': today_datatime,
+#         'price': 20.0077,
+#     }
+
+#     return render(request, "main/index.html", context)
+
+
+def learn_django(request):
+
+    context = {
+        "stu1": {"name": "kshittiz", "age": 80},
+        "stu2": {"name": "Ram", "age": 22},
+        "stu3": {"name": "Unnati", "age": 21},
+    }
+    return render(request, "main/index.html", context)
