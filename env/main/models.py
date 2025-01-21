7b9b15b686f6fe285550e7811ecb4eed4fa942e9
kshittiz101 <chaudharykshittiz950@gmail.com>
2025-01-21 21:28:40 +0545

    env setup complete

--- env/main/models.py
+++ env/main/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Student(models.Model):
+  name = models.CharField(max_length=200)
+  age = models.IntegerField()
+  address = models.CharField(max_length=200)
+
+  def __str__(self):
+    return self.name
